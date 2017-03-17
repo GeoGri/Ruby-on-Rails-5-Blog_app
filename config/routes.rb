0ea6c9ce677b8a98668d23299af3ba2619bed231
GeoGri <grzegorz1wojda@gmail.com>
2017-03-17 18:50:10 +0100

    Start static sites

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "posts#index"
+
+  get '/about', to: 'static_pages#about'
+
   devise_for :users
+
   resources :posts do 
     resources :comments
   end
 
-  root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
